981a94ed5e56756f5874f9a422389d75b470a134
yasinabdmahmood <abd181103@gmail.com>
2022-07-25 17:00:21 +0300

    Fix linter error for js file

--- myScript.js
+++ myScript.js
@@ -1,47 +1,41 @@
-//create ul element
-function createList(){
-    let el=document.createElement('div');
-    el.id='div-js'
-let ul=document.createElement('ul');
-ul.id='ul-js'
-let arr=['Portfolio','About me','Contact']
-let link=['section1','section2','section3'] /*this needs to be modified
-it should contain the Id of sections*/
+// create ul element
+function createList() {
+  const el = document.createElement('div');
+  el.id = 'div-js';
+  const ul = document.createElement('ul');
+  ul.id = 'ul-js';
+  const arr = ['Portfolio', 'About me', 'Contact'];
 
-let li=document.createElement('li');
-li.className='list-item-js  close-tap';
-let img=document.createElement('img')
-img.setAttribute('src','/img/x-lg.svg')
-img.className='close-tap-img'
-li.appendChild(img)
-ul.appendChild(li)
-for(let i of arr){
-    let li=document.createElement('li');
-    li.className='list-item-js';
-    let a=document.createElement('a');
-    a.setAttribute('href','#'+i)
-    let text = document.createTextNode(i);
-    a.appendChild(text)
+  const li = document.createElement('li');
+  li.className = 'list-item-js  close-tap';
+  const img = document.createElement('img');
+  img.setAttribute('src', '/img/x-lg.svg');
+  img.className = 'close-tap-img';
+  li.appendChild(img);
+  ul.appendChild(li);
+  for (let i = 0; i < arr.length; i += 1) {
+    const li = document.createElement('li');
+    li.className = 'list-item-js';
+    const a = document.createElement('a');
+    a.setAttribute('href', `#${arr[i]}`);
+    const text = document.createTextNode(arr[i]);
+    a.appendChild(text);
     li.appendChild(a);
-    ul.appendChild(li)
-}
+    ul.appendChild(li);
+  }
 
-el.appendChild(ul);
+  el.appendChild(ul);
 
-document.body.appendChild(el);
-let cssLink=document.createElement('link');
-cssLink.setAttribute('rel','stylesheet');
-cssLink.setAttribute('href','styles2.css'); //modify
-document.head.appendChild(cssLink)
+  document.body.appendChild(el);
+  const cssLink = document.createElement('link');
+  cssLink.setAttribute('rel', 'stylesheet');
+  cssLink.setAttribute('href', 'styles2.css'); // modify
+  document.head.appendChild(cssLink);
 
-el.onclick=()=>{
+  el.onclick = () => {
     el.remove();
     cssLink.remove();
-    
+  };
 }
-}
 
-document.getElementById('humberger-icon').onclick = createList;  //modify
-
-
-
+document.getElementById('humberger-icon').onclick = createList; // modify
