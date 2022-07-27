bc92ffda0c4419ac3e15dda7348e50b7b817b6cc
yasinabdmahmood <abd181103@gmail.com>
2022-07-27 09:13:46 +0300

    Fix linter error

--- myScript.js
+++ myScript.js
@@ -97,7 +97,7 @@ const projects = [
     linkToLive: 'https://github.com/',
     linkToSource: 'https://github.com/yasinabdmahmood/portfolio-website',
 
-  },{
+  }, {
     title: 'Multi Post Stories',
     discription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
     featureImg: './img/card1-project-img.png',
@@ -105,7 +105,7 @@ const projects = [
     linkToLive: 'https://github.com/',
     linkToSource: 'https://github.com/yasinabdmahmood/portfolio-website',
 
-  }
+  },
 ];
 
 function createProjectPopUp() {
