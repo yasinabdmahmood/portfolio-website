4bc889d2bd8fe3878d2141dd9253fbefa9377095
yasinabdmahmood <abd181103@gmail.com>
2022-07-28 18:07:08 +0300

    Fix linter error

--- myScript.js
+++ myScript.js
@@ -225,56 +225,50 @@ form.addEventListener('submit', (event) => {
   form.querySelector('.error').innerHTML = message;
 });
 
-
-/////////// Storage Project /////////////////
-let id=['full-name','email','textarea','fname','lname','Email-Address','text-area'];
-let data={mobileFullName:"",mobileEmail:"",mobileText:"",desktopFirstName:"",desktopLastName:"", desktopEmail:"",desktopText:""
+/// //////// Storage Project /////////////////
+const id = ['full-name', 'email', 'textarea', 'fname', 'lname', 'Email-Address', 'text-area'];
+let data = {
+  mobileFullName: '', mobileEmail: '', mobileText: '', desktopFirstName: '', desktopLastName: '', desktopEmail: '', desktopText: '',
+};
+function getInputFields() {
+  data.mobileFullName = document.getElementById('full-name').value;
+  data.mobileEmail = document.getElementById('email').value;
+  data.mobileText = document.getElementById('textarea').value;
+  data.desktopFirstName = document.getElementById('fname').value;
+  data.desktopLastName = document.getElementById('lname').value;
+  data.desktopEmail = document.getElementById('Email-Address').value;
+  data.desktopText = document.getElementById('text-area').value;
+  return data;
 }
-if(!localStorage.getItem('data')) {
-   copyInputFieldsToLocaStorage();
-    console.log("Does not exist")
-    
-  } else {
-    copyLocalStorageToInputFields();
-    console.log("exist")
-  }
 
-  function copyInputFieldsToLocaStorage(){
-    console.log('data is ')
-    data=getInputFields();
-    console.log('data is ')
-    console.log(data)
-    localStorage.setItem('data',JSON.stringify(data))
-    copyLocalStorageToInputFields();
-  }
+function setInputFields() {
+  document.getElementById('full-name').value = data.mobileFullName;
+  document.getElementById('email').value = data.mobileEmail;
+  document.getElementById('textarea').value = data.mobileText;
+  document.getElementById('fname').value = data.desktopFirstName;
+  document.getElementById('lname').value = data.desktopLastName;
+  document.getElementById('Email-Address').value = data.desktopEmail;
+  document.getElementById('text-area').value = data.desktopText;
+}
 
-  function getInputFields(){
-    data.mobileFullName=document.getElementById('full-name').value;
-    data.mobileEmail=document.getElementById('email').value;
-    data.mobileText=document.getElementById('textarea').value;
-    data.desktopFirstName=document.getElementById('fname').value;
-    data.desktopLastName=document.getElementById('lname').value;
-    data.desktopEmail=document.getElementById('Email-Address').value;
-    data.desktopText=document.getElementById('text-area').value;
-    return data;
-  }
+function copyLocalStorageToInputFields() {
+  data = JSON.parse(localStorage.getItem('data'));
+  setInputFields();
+}
 
-  function copyLocalStorageToInputFields(){
-    data=JSON.parse(localStorage.getItem('data'));
-    setInputFields();
-  }
-  function setInputFields(){
-    
-    document.getElementById('full-name').value =data.mobileFullName;
-    document.getElementById('email').value= data.mobileEmail;
-    document.getElementById('textarea').value= data.mobileText;
-    document.getElementById('fname').value= data.desktopFirstName;
-    document.getElementById('lname').value= data.desktopLastName;
-    document.getElementById('Email-Address').value= data.desktopEmail;
-    document.getElementById('text-area').value= data.desktopText;
-  }
+function copyInputFieldsToLocaStorage() {
+  data = getInputFields();
 
-  for(let i=0;i<id.length;i+=1){
-    document.getElementById(id[i]).onchange=copyInputFieldsToLocaStorage;
-  }
-  
+  localStorage.setItem('data', JSON.stringify(data));
+  copyLocalStorageToInputFields();
+}
+
+if (!localStorage.getItem('data')) {
+  copyInputFieldsToLocaStorage();
+} else {
+  copyLocalStorageToInputFields();
+}
+
+for (let i = 0; i < id.length; i += 1) {
+  document.getElementById(id[i]).onchange = copyInputFieldsToLocaStorage;
+}
